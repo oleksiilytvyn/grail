fa06c2f2d04a6f481e8fcb600669cec768ff6656
Alex Litvin <programer95@gmail.com>
2015-10-29 02:41:43 +0200

    increment version number setup fixes new resources file Info.plist aded

--- grail/__init__.py
+++ grail/__init__.py
@@ -18,7 +18,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = '0.9.1'
+__version__ = '0.9.2'
 
 import sys
 import os

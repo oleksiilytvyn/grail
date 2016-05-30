619e4b12c23801359cd76d69dfd6b0c41ba4884b
Alex Litvin <programer95@gmail.com>
2016-05-30 11:36:15 +0300

    NEW: navigate to search using shortcut - Ctrl+` (Cmd+`) NEW: navigate to playlist using shortcut - Ctrl+1 (Cmd+1)
    FIX: wrong label position in output osc preferences
    FIX: new styles for list of playlist dialog

--- grail/dialogs/balloon_dialog.py
+++ grail/dialogs/balloon_dialog.py
@@ -27,65 +27,71 @@ from grail.utils import *
 
 class BalloonDialog(QDialog):
 
-    def __init__( self, parent=None ):
+    def __init__(self, parent=None):
 
-        super(BalloonDialog, self).__init__( parent )
+        super(BalloonDialog, self).__init__(parent)
 
-        self.setWindowFlags( Qt.Widget | Qt.FramelessWindowHint | Qt.X11BypassWindowManagerHint )
-        self.setAttribute( Qt.WA_NoSystemBackground, True )
-        self.setAttribute( Qt.WA_TranslucentBackground, True )
+        self._backgorund_color = QColor(255, 255, 255)
 
-        self.installEventFilter( self )
+        self.setWindowFlags(Qt.Widget | Qt.FramelessWindowHint | Qt.X11BypassWindowManagerHint)
+        self.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+
+        self.installEventFilter(self)
 
         if not PLATFORM_MAC:
             effect = QGraphicsDropShadowEffect()
-            effect.setBlurRadius( 12 )
-            effect.setColor( QColor(0, 0, 0, 126) )
-            effect.setOffset( 0 )
+            effect.setBlurRadius(12)
+            effect.setColor(QColor(0, 0, 0, 126))
+            effect.setOffset(0)
 
-            self.setGraphicsEffect( effect )
+            self.setGraphicsEffect(effect)
 
-        self.setContentsMargins( 12, 12, 12, 19 )
+        self.setContentsMargins(12, 12, 12, 19)
 
-    def paintEvent( self, event ):
+    def paintEvent(self, event):
 
         roundness = 7
         side = 7
 
         painter = QPainter()
-        painter.begin( self )
+        painter.begin(self)
         painter.save()
 
-        painter.setRenderHint( QPainter.Antialiasing )
-        painter.setPen( Qt.NoPen )
-        painter.setBrush( QColor(255, 0, 0, 127) )
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor(255, 0, 0, 127))
 
-        points = [QPointF( self.width() / 2, self.height() - 12 ),
-                  QPointF( self.width() / 2 - side, self.height() - side - 12 ),
-                  QPointF( self.width() / 2 + side, self.height() - side - 12 )]
-        triangle = QPolygonF( points )
+        points = [QPointF(self.width() / 2, self.height() - 12),
+                  QPointF(self.width() / 2 - side, self.height() - side - 12),
+                  QPointF(self.width() / 2 + side, self.height() - side - 12)]
+        triangle = QPolygonF(points)
 
         rounded_rect = QPainterPath()
-        rounded_rect.addRoundedRect( 12, 12, self.width() - 24, self.height() - side - 24, roundness, roundness )
-        rounded_rect.addPolygon( triangle )
+        rounded_rect.addRoundedRect(12, 12, self.width() - 24, self.height() - side - 24, roundness, roundness)
+        rounded_rect.addPolygon(triangle)
 
-        painter.setOpacity( 1 )
-        painter.fillPath( rounded_rect, QBrush( Qt.white) )
+        painter.setOpacity(1)
+        painter.fillPath(rounded_rect, QBrush(self._backgorund_color))
 
         painter.restore()
         painter.end()
 
-    def eventFilter( self, target, event ):
+    def eventFilter(self, target, event):
         if event.type() == QEvent.WindowDeactivate:
             self.hide()
 
-        return QObject.eventFilter( self, target, event )
+        return QObject.eventFilter(self, target, event)
 
-    def sizeHint( self ):
+    def sizeHint(self):
         return QSize(300, 300)
 
-    def showAt( self, point ):
+    def showAt(self, point):
 
         self.show()
         self.raise_()
-        self.move( point.x() - self.width() / 2, point.y() - self.height() + 12 )
+        self.move(point.x() - self.width() / 2, point.y() - self.height() + 12)
+
+    def setBackgroundColor(self, color):
+
+        self._backgorund_color = color
